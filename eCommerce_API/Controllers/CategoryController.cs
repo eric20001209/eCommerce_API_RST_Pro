10db55d8481733940f610acd64ee7542bb5cde4b
eric2000.c <eric2000.c@gmail.com>
2020-11-23 14:23:32 +1300

    ..

--- eCommerce_API/Controllers/CategoryController.cs
+++ eCommerce_API/Controllers/CategoryController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using eCommerce_API.Data;
+using eCommerce_API.Services;
 
 namespace eCommerce_API.Controllers
 {
@@ -13,9 +14,11 @@ namespace eCommerce_API.Controllers
     public class CategoryController : ControllerBase
     {
         rst374_cloud12Context _context;//= new rst374_cloud12Context();
-        public CategoryController(rst374_cloud12Context context)
+        private readonly ISetting _isettings;
+        public CategoryController(rst374_cloud12Context context, ISetting isettings)
         {
             _context = context;
+            _isettings = isettings;
         }
 
         [HttpGet()]
@@ -23,17 +26,20 @@ namespace eCommerce_API.Controllers
         {
             var list = _context.CodeRelations
                 .Where(c => c.IsWebsiteItem == true && c.OnLineRetail == true && c.Skip == false)
-//              .Where(c=>c.Skip == false)
                 .Where(c => !String.IsNullOrEmpty(c.Cat))
                 .Where(c => cat != null ? c.Cat == cat : true)
                 .Where(c => scat != null ? c.SCat == scat : true)
                 .Where(c => sscat != null ? c.SsCat == scat : true)
-                .Select(c => new
-                {
+                .Join(_context.StockQty.Where(sq => sq.BranchId == _isettings.getOnlineShopId() && sq.Qty > 0),
+                c=>c.Code,
+                sq => sq.Code,
+                (c,sq) => new {
                     c.Cat,
                     c.SCat,
                     c.SsCat
-                }).Distinct().ToList();
+                })
+                .Distinct()
+                .ToList();
 
             return Ok(list);
         }
